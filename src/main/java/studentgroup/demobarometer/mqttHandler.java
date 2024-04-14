eed98b8cbb9c8dc76e809cf207361572549388f0
VanyaTretiak <vanya1112tretiak@gmail.com>
2024-04-14 21:07:48 +0300

    sdf2.0

--- src/main/java/studentgroup/demobarometer/mqttHandler.java
+++ src/main/java/studentgroup/demobarometer/mqttHandler.java
@@ -1,74 +1,7 @@
-//package studentgroup.demobarometer;
-////import org.eclipse.paho.client.mqttv3.MqttClient;
-////import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
-////import org.eclipse.paho.client.mqttv3.MqttException;
-////import org.eclipse.paho.client.mqttv3.MqttMessage;
-////import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
-//import com.hivemq.client.mqtt.MqttClient;
-//import com.hivemq.client.mqtt.datatypes.MqttQos;
-//import com.hivemq.client.mqtt.mqtt3.Mqtt3AsyncClient;
-//
-//import java.util.UUID;
-//
-//public class mqttHandler {
-//
-//    private static final Mqtt3AsyncClient client = MqttClient.builder()
-//            .useMqttVersion3()
-//            .identifier(UUID.randomUUID().toString())
-//            .automaticReconnectWithDefaultConfig()
-//            .serverHost("2661f15a40284af4a2caa13a37aa9772.s1.eu.hivemq.cloud")
-//            .serverPort(8883)
-//            .sslWithDefaultConfig()
-//            .buildAsync();
-//    public void connect(String username, String password){
-//        client.connectWith()
-//                .simpleAuth()
-//                .username(username)
-//                .password(password.getBytes())
-//                .applySimpleAuth()
-//                .send()
-//                .whenComplete((connAck, throwable) -> {
-//                    // Handle connection complete
-//                    System.out.println("Successfully connected:)");
-//                });
-//    }
-//    public void publish(String message, String topic){
-//        client.publishWith()
-//                .topic("IoTBar/" + topic)
-//                .payload(message.getBytes())
-//                .qos(MqttQos.AT_MOST_ONCE)
-//                .send()
-//                .whenComplete((mqtt3Publish, throwable) -> {
-//                    if (throwable != null) {
-//                        // Handle failure to publish
-//                    } else {
-//                        System.out.println("Message:" + message + " successfully published");
-//                    }
-//                });
-//    }
-//    public void subscribe(String topic){
-//        client.subscribeWith()
-//                .topicFilter(topic)
-//                .callback(publish -> {
-//                    // Process the received message
-//                })
-//                .send()
-//                .whenComplete((subAck, throwable) -> {
-//                    if (throwable != null) {
-//                        // Handle failure to subscribe
-//                    } else {
-//                        System.out.println("Successfully subscribed to "+topic);
-//                    }
-//                });
-//    }
-//}
-
-
 package studentgroup.demobarometer;
 
 import com.hivemq.client.mqtt.datatypes.MqttQos;
 import com.hivemq.client.mqtt.mqtt5.Mqtt5Client;
-
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
